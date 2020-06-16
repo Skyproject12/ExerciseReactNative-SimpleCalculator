8b97642a3081f786c8d93a43d84df9b4ac72e750
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-06-16 11:15:10 +0700

    strcuture simple react native

---
+++ app/component/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import Header from './Heaeder';
+
+export class index extends Component {
+  render() {
+    return (
+      <View>
+        <Header />
+      </View>
+    );
+  }
+}
+
+export default index;
